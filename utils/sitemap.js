33ecb0094c2b32bbf260301988441e6b01092289
Avoo <git@avoonix.com>
2022-06-12 20:46:04 +0200

    redirect

--- utils/sitemap.js
+++ utils/sitemap.js
@@ -53,6 +53,7 @@ async function generateSiteMap() {
       "/bdsm-list /kinks",
       "/fetish /kinks",
       "/fetishes /kinks",
+      "/nsfw-gallery /gallery/tagged/yiff",
       ...Object.entries(meta.gallery).map(([path, g]) => `/gallery/${g.id} /gallery/${getSlug({ path, id: g.id })}`),
       ...Object.entries(meta.gallery).map(([path, g]) => `/${g.id} /gallery/${getSlug({ path, id: g.id })}`),
       ...Object.entries(meta.gallery).map(([path, g]) => `/images/${g.oldPath} https://i.avoonix.com/images/${path}`),
